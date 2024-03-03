975c83adfa366328bdcf85e9efe2612541c6f5bd
Иорданов Павел Алексеевич <paiordanov@edu.hse.ru>
2024-03-03 17:03:13 +0300

    Оutput in string format

--- tle.py
+++ tle.py
@@ -7,7 +7,16 @@ response = requests.get(url)
 if response.status_code == 200:
     data = response.text
     
-    print(data)
-    
+    # Разделим данные на строки
+    lines = data.strip().split('\n')
+
+    # Объединим каждую группу из трех строк в одну строку
+    grouped_lines = [lines[i] + '\n' + lines[i+1] + '\n' + lines[i+2] for i in range(0, len(lines), 3)]
+
+    # Выведем каждую группу в требуемом формате
+    for group in grouped_lines:
+        
+        print(f'"""{group}')
+        print('"""')
 else:
     print("Ошибка при получении данных:", response.status_code)
