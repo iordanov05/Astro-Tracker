9c9a79e5a570275103246ef40bd4f1d307be05f6
Иорданов Павел Алексеевич <paiordanov@edu.hse.ru>
2024-03-05 22:16:44 +0300

    Added data to the array

--- tle.py
+++ tle.py
@@ -1,4 +1,5 @@
 import requests
+import textwrap
 
 url = "https://r4uab.ru/satonline.txt"
 
@@ -12,11 +13,17 @@ if response.status_code == 200:
 
     # Объединим каждую группу из трех строк в одну строку
     grouped_lines = [lines[i] + '\n' + lines[i+1] + '\n' + lines[i+2] for i in range(0, len(lines), 3)]
-
-    # Выведем каждую группу в требуемом формате
+    
+    # Создаем пустой список для хранения групп строк
+    grouped_lines_array = []
+    
+    # Добавим каждую строку в массив с сохранением форматирования
     for group in grouped_lines:
         
-        print(f'"""{group}')
-        print('"""')
+        satellite = (f'"""{group}\n"""')
+        print(satellite)
+        grouped_lines_array.append(textwrap.dedent(satellite).strip())
+        
+    
 else:
     print("Ошибка при получении данных:", response.status_code)
