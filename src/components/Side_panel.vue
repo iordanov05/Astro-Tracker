04c6eb35ccb9d3ee4b62f2b31f02471c8593c372
DaryaVorontsova <vorontsova.darya.nikolaevna@gmail.com>
2024-03-12 00:10:43 +0300

    Adding information about each satellite to the side panel

--- src/components/Side_panel.vue
+++ src/components/Side_panel.vue
@@ -1,16 +1,98 @@
 <script>
+import axios from 'axios'
+
     export default{
         data(){
             return{
                 showButton: true,
-                selectedOption: ''
+                selectedOption: '',
+                jsonData: null,
+                names: []
             }
         },
-        methods: {
+        mounted(){
+                axios.get('/src/test_1.json')
+                .then(response =>{
+                    this.jsonData = response.data;
+                    this.parseStringToArr(this.jsonData);
+                });
+        },
+        computed: {
+            argPerigee(){
+                return "<b>Аргумент перигея: </b>" + this.jsonData[this.selectedOption - 1]["arg_perigee"];
+            },
+            bStar(){
+                return "<b>Коэффицент торможения: </b>" + this.jsonData[this.selectedOption - 1]["bstar"];
+            },
+            checkSumLine1(){
+                return "<b>Контрольная сумма 1: </b>" + this.jsonData[this.selectedOption - 1]["checksum"];
+            },
+            checkSumLine2(){
+                return "<b>Контрольная сумма 2: </b>" + this.jsonData[this.selectedOption - 1]["checksum_2"];
+            },
+            classific(){
+                return "<b>Классификация: </b>" + this.jsonData[this.selectedOption - 1]["classification"];
+            },
+            eccentr(){
+                return "<b>Эксцентриситет: </b>" + this.jsonData[this.selectedOption - 1]["eccentricity"];
+            },
+            numbElementSet(){
+                return "<b>Номер набора элементов: </b>" + this.jsonData[this.selectedOption - 1]["element_set_number"];
+            },
+            typeEpheremide(){
+                return "<b>Тип эферемиды (орбитальная модель): </b>" + this.jsonData[this.selectedOption - 1]["ephemeris_type"];
+            },
+            epochIs(){
+                return "<b>Эпоха: </b>" + this.jsonData[this.selectedOption - 1]["epoch"];
+            },
+            epochYear(){
+                return "<b>Год эпохи: </b>" + this.jsonData[this.selectedOption - 1]["epoch_year"];
+            },
+            inclin(){
+                return "<b>Наклонение: </b>" + this.jsonData[this.selectedOption - 1]["inclination"];
+            },
+            numbIss(){
+                return "<b>Номер ИСЗ спутника в базе данных NORAD: </b>" + this.jsonData[this.selectedOption - 1]["iss_number"];
+            },
+            launchFragm(){
+                return "<b>Фрагмент запуска: </b>" + this.jsonData[this.selectedOption - 1]["launch_fragment"];
+            },
+            launchNumb(){
+                return "<b>Номер запуска в году: </b>" + this.jsonData[this.selectedOption - 1]["launch_number"];
+            },
+            launchYear(){
+                return "<b>Год запуска: </b>" + this.jsonData[this.selectedOption - 1]["launch_year"];
+            },
+            meanAnomal(){
+                return "<b>Средняя аномалия: </b>" + this.jsonData[this.selectedOption - 1]["mean_anomaly"];
+            },
+            meanMovement(){
+                return "<b>Среднее движение (оборотов в день): </b>" + this.jsonData[this.selectedOption - 1]["mean_motion"];
+            },
+            derivativeFirst(){
+                return "<b>Первая производная среднего движения по времени: </b>" + this.jsonData[this.selectedOption - 1]["mean_motion_derivative"];
+            },
+            derivativeSecond(){
+                return "<b>Вторая производная среднего движения по времени: </b>" + this.jsonData[this.selectedOption - 1]["second_derivative"];
+            },
+            orbitNumb(){
+                return "<b>Номер витка: </b>" + this.jsonData[this.selectedOption - 1]["orbit_number"];
+            },
+            rightAscension(){
+                return "<b>Прямое восхождение восходящего узла: </b>" + this.jsonData[this.selectedOption-1]["raan"]
+            }
+        },
+         methods: {
             hideButton(){
                 this.showButton = false
+            },
+            parseStringToArr(inputData){
+                for (let index = 0; index < inputData.length; index++){
+                    const item = inputData[index];
+                    this.names.push(item["satellite_name"]);
+                }
             }
-        }
+        },
     }
 </script>
 
@@ -21,7 +103,7 @@
             <div className="select_wrapper" >
                 <select v-model="selectedOption" className="custom_select" name="" id="">
                     <option disabled value="">Выберите спутник</option>
-                    <option value="0">1</option>
+                    <option v-for="(el, index) in names" :key="index" :value="index+1">{{ el }}</option>
                 </select>  
             </div>
             <button 
@@ -32,6 +114,35 @@
             </button>
             <button disabled v-else className="button_get_data button_text">Нажмите для получения данных</button>
             <img v-if="showButton" className='satellite_img' src="/src/components/icons/Satellite.svg" alt=""> 
+            <div v-if="selectedOption!='' && !showButton" className="main_text">
+                <div className="fixed_up">
+                    <h3>ИНФОРМАЦИЯ О СПУТНИКЕ</h3>
+                </div>
+                <div className="line"></div>
+                <ul className="scrolled">
+                    <li v-html="numbIss"></li>
+                    <li v-html="classific"></li>
+                    <li v-html="launchYear"></li> 
+                    <li v-html="launchNumb"></li> 
+                    <li v-html="launchFragm"></li>
+                    <li v-html="epochYear"></li>  
+                    <li v-html="epochIs"></li>
+                    <li v-html="derivativeFirst"></li>
+                    <li v-html="derivativeSecond"></li>  
+                    <li v-html="bStar"></li>
+                    <li v-html="typeEpheremide"></li>
+                    <li v-html="numbElementSet"></li>
+                    <li v-html="checkSumLine1"></li>
+                    <li v-html="inclin"></li>
+                    <li v-html="rightAscension"></li>  
+                    <li v-html="eccentr"></li>
+                    <li v-html= "argPerigee" ></li>
+                    <li v-html="meanAnomal"></li>
+                    <li v-html="meanMovement"></li> 
+                    <li v-html="orbitNumb"></li>  
+                    <li v-html="checkSumLine2"></li>
+                </ul>
+            </div>  
         </div>
     </div>
 </template>
@@ -41,7 +152,7 @@
 .side_panel_wrapper{
     background: #bbe1ea;
     border-radius: 25px;
-    width: 100%;
+    width: 80%;
     padding: 10px 15px;
     display: flex;
     flex-direction: column;
@@ -136,4 +247,68 @@
     opacity: 0.7;
 }
 
+.main_text li{
+    font-family: 'Journal Sans', sans-serif;
+    font-size: 15px;
+    color: #fff;
+}
+
+.main_text ul{
+    padding-inline-start: 20px;
+}
+
+.main_text{
+    background: #17364b;
+    border: 1px solid #000;
+    border-radius: 20px;
+    gap: 2px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    height: 420px;
+}
+
+.fixed_up{
+    z-index: 1; 
+    margin: 10px 20px;
+}
+
+.fixed_up h3{
+    margin: 0;
+    font-family: 'Berlin Sans', sans-serif;
+    font-size: 18px;
+    text-align: center;
+    color: #fff;
+}
+
+.scrolled{
+    height: 100%;
+    overflow-y: auto;
+    margin: 10px;
+}
+
+::-webkit-scrollbar {
+    width: 11px; 
+    margin-right: -10px;
+}
+
+::-webkit-scrollbar-track {
+    background: rgba(217, 217, 217, 0.71);
+    border-radius: 25px;
+}
+
+::-webkit-scrollbar-thumb {
+    background: rgba(24, 27, 32, 0.53);
+    border-radius: 25px; 
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+.line{
+    width: 100%;
+    border-top: 6px solid #bbe1ea; 
+}
 </style>
